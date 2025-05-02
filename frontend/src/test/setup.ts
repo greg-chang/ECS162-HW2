97ee8c840c801f5790668b8542a34aa20306fb58
nabrego <93846936+nabrego@users.noreply.github.com>
2025-05-01 18:00:22 -0700

    created api test

---
+++ frontend/src/test/setup.ts
@@ -0,0 +1,14 @@
+import { beforeAll, afterAll, vi } from 'vitest';
+import { JSDOM } from 'jsdom';
+
+const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
+  url: 'http://localhost',
+});
+
+vi.stubGlobal('document', dom.window.document);
+vi.stubGlobal('window', dom.window);
+vi.stubGlobal('navigator', dom.window.navigator);
+
+afterAll(() => {
+  vi.unstubAllGlobals();
+}); 
